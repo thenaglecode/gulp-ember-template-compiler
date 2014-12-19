30c616efb65342430a1f654d41c53fd37de92dc7
Jared Nagle <jared.nagle@acegroup.com>
2014-12-19 14:23:12 +1100

    inital commit for alpha

---
+++ index.js
@@ -0,0 +1,5 @@
+/* jshint devel: true */
+
+module.exports = function() {
+    throw new PluginError('Nothing has been implemented yet, please wait until the next release');
+};
---
+++ test/test.js
@@ -0,0 +1,7 @@
+/* jshint devel: true */
+
+describe('no tests yet' , function(){
+    'use strict';
+
+    console.log('not testing anything here yet...');
+});
